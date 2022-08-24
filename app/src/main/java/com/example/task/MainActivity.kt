ba76a5798f7cfa5b6c5771d84111b5ab66e5027b
Walid Zain <walidzeno@gmail.com>
2022-08-25 00:54:01 +0200

    Updated UI

--- app/src/main/java/com/example/task/MainActivity.kt
+++ app/src/main/java/com/example/task/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.task
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.task.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.phoneCodeTextView.setText(binding.countryPicker.selectedCountryCodeWithPlus.toString())
+
+        binding.countryPicker.setOnCountryChangeListener {
+            binding.phoneCodeTextView.setText(binding.countryPicker.selectedCountryCodeWithPlus.toString())
+        }
     }
 }
